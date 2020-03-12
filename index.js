586ec20e743fd1c03637eecc7f4fb8d1cbd09190
Mariano Guerra <marianoguerrao@gmail.com>
2020-03-11 18:30:05 -0600

    Se descargó 'cowsay' y 'yodasay' y se importó en el doc index,js para su uso

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const cowsay = require ('cowsay')
+/* para referirnos a modulos externos sólo hay que hacer referencia del nombre del paquete. 
+También se puede consultar en la documentacion del modulo o paquete*/
+
+console.log(cowsay.say({
+	text : "Mooooooo",
+	e : "x X",
+	T : " U "
+}))
+
+const yodasay = require ('yodasay')
+
+console.log(yodasay.say({
+  text : 'Muy chingón, eres tú.'
+}));
