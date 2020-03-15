8d01874eda3935e23469ee4ccaaa20843f06bcd4
Mariano Guerra <marianoguerrao@gmail.com>
2020-03-15 13:23:40 -0600

    Se agregó una promesa async-await al archivo index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require('./src/call')
+
+//call.greet('Mariano')
+
+/*call.withPromise('Mariano', 'Guerra')
+  .then(n => console.log(n))*/
+
+async function callWithPromise(){ // con async y await se garantiza que se resuelvan las promesas antes de regresar el resultado
+  const fullName = await call.withPromise('Mariano', 'Guerra')
+  console.log(fullName)
+} 
+
+callWithPromise('Mariano', 'Guerra')
