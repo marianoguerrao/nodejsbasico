556136830f0f8c35dbad5bc03a7d73ab10758b11
Mariano Guerra <marianoguerrao@gmail.com>
2020-03-17 17:43:46 -0600

    se importaron las funciones de errors.js y handling.js para probarlos en el debugger

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const errors = require ('./src/errors')
+const handling = require ('./src/handling')
+
+try {
+  errors.standardErr.range()
+
+} catch(err){
+  console.log(`Error: ${err}`)
+}
+
+try{
+  handling.sync ()
+
+} catch(error){
+  console.log(`Ha ocurrido un error: ${error} `)
+}
+
+handling.errorFirstCallbackWrong()
