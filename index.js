25be66a49af37a6770cb0485acea1e33f349ec59
Mariano Guerra <marianoguerrao@gmail.com>
2020-03-17 17:24:11 -0600

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const {
-    from0to,
-    asyncFrom0to
-} = require("./src/counter");
-
-// Llamadas a funciones
-asyncFrom0to(10,3);
-from0to(10, 2);
+/**
+ * 
+ * *************
+ * Comience aqui
+ * *************
+ * 
+ */
