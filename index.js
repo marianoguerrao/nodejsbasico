e542a70c993713461b9c3c974a040b93c2eec61f
Mariano Guerra <marianoguerrao@gmail.com>
2020-03-17 14:06:23 -0600

    Se creó una función en index.js para poder ejecutar la función esctira en script.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const cp = require('child_process')
+
+function execCommand(command){
+  cp.exec(command, (err, stdout, stderr) =>{
+    if (err) {
+      console.log(`Error: ${err} `)
+      return
+    }
+    if(stdout) console.log(`Standard out: ${stdout}` )
+    if(stderr) console.log(`Standard out: ${stderr}` )
+  })
+}
+
+//execCommand('dir')
+execCommand('node ./src/script --base=5')
