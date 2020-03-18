f043b36eb099b2a069dbcfa77437e8c013497ee6
Mariano Guerra <marianoguerrao@gmail.com>
2020-03-17 18:08:53 -0600

    Se invocaron las funciones de greet.emit para conocer como funcionan los eventos

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const greet = require ('./src/greet')
+
+/*.emit invoca a la función 'clap' del archivo greet declarada con myEmmiter.on */
+greet.emit('clap')
+
+greet.emit('greet', 'Mariano')
+
+greet.emit('shout', 'NO ENTIENDO NI MADRES')
+
+greet.emit('call', 'Mariano', (name)=>{
+  console.log(`Estamos llamando a: `, name)
+})
