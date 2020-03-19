2677f3f9c2ed1ee08af7e121cbe3693abac6516b
Mariano Guerra <marianoguerrao@gmail.com>
2020-03-19 17:07:25 -0600

    Se levantó un servidor local y se conectó a un documento html 'my_page'

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- *  
- */
+const http = require ('http') // recurso local
+const fs = require ('fs')// recurso local
+const port = 3000
+
+const server = http.createServer((req, res) =>{
+  fs.readFile('./resources/my_page.html', (err, data)=>{
+    if (err) { 
+      console.error(err)
+      return 
+    }
+    res.end(data)
+  })
+})
+
+console.log(`Servidor iniciado en el puerto ${port}`)
+server.listen(port)
