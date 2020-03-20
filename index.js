1c8f0db01c5693847d61203065303eb127d68d2e
Mariano Guerra <marianoguerrao@gmail.com>
2020-03-19 18:28:16 -0600

    Se configuró package.json para poder iniciar el servidor con nodemon y se inició un servidor con express devolviendo una base de datos en json

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const { PORT } = require("./config");
 
-require("./routes/views")(app);
 require("./routes/api")(app);
+require("./routes/views")(app);
+/* require...views deberá estar al final porque dentro del documento se encuentra
+la ruta '*' que el explorador tiene que leer hasta el final. Si ponemos el require de /api después ya no se leeran las rutas
+establecidas en ese documento */
 
 function init(){
     console.log("Iniciando instancia de Express...");
