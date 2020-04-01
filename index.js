b7cafd03a43c507aa21d9be11c9982b53641f931
Mariano Guerra <marianoguerrao@gmail.com>
2020-04-01 13:45:14 -0600

    se escribió el código para levantar el servidor en index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-/**
- * 
- * ***************
- *  Comience aqui
- * ***************
- * 
- */
+const server = require ('http').server()
+const io= require ('socket.io')(server)
+const port = require ('./config').SERVERPORT // datos localizados en config.json
+
+const banner = `
+******************************
+  Basic Node JS Course
+  Tic Tac Toe Project
+******************************
+Status: Online
+Listening on port: ${port}
+`
+
+io.onconnection('connection', (socket) =>{
+  socket.on('register', (user) =>{
+    console.log(`El usuario ${user.name} ha sido registrado con éxito`)
+  })
+})
+
+server.listen(port, ()=>{
+  console.info(banner)
+})
